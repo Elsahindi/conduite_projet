e30b06d316982f9d384bc1a610f06a2b0b55878c
millillitre <130367741+millillitre@users.noreply.github.com>
2024-10-14 13:36:24 +0200

    TEST_Creation

---
+++ src/test/java/UserTest.java
@@ -0,0 +1,21 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+class UserTest {
+
+    @Test
+    void getId() {
+    }
+
+    @Test
+    void getPswd() {
+    }
+
+    @Test
+    void createUser() {
+    }
+
+    @Test
+    void login() {
+    }
+}
