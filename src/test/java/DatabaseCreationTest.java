12dc85dc2dfe341d8f5ff627f828a6bdaeb6ce3b
millillitre <130367741+millillitre@users.noreply.github.com>
2024-11-06 13:04:25 +0100

    DatabaseCreation Test

--- src/test/java/DatabaseCreationTest.java
+++ src/test/java/DatabaseCreationTest.java
@@ -1,14 +1,36 @@
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
+
+import java.sql.Connection;
+import java.sql.SQLException;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class DatabaseCreationTest {
 
-    @Test
-    void getConnection() {
-    }
+    private DatabaseCreation databaseCreation;
+
+//    // to test if the connection is closed after each test
+//    @AfterEach
+//    void tearDown() throws SQLException {
+//        if (databaseCreation != null && databaseCreation.getConnection() != null) {
+//            databaseCreation.getConnection().close();
+//        }
+//    }
 
     @Test
+    void getConnection() throws SQLException {
+        databaseCreation = DatabaseCreation.getInstance();
+        Connection connection = databaseCreation.getConnection();
+        assertNotNull(connection, "Connection should not be null");
+        assertFalse(connection.isClosed(), "Connection should be open");
+    }
+
+    // to test if we hhave only one instance of connection
+    @Test
     void getInstance() {
+        DatabaseCreation instance1 = DatabaseCreation.getInstance();
+        DatabaseCreation instance2 = DatabaseCreation.getInstance();
+        assertSame(instance1, instance2, "It should be only one DatabaseCreation");
     }
 }
